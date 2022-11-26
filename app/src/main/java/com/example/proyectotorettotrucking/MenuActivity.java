9ed39dc62339f831fbea0652e9d4fbb82f6e3487
Islu <luigui.angel07@hotmail.com>
2022-11-26 13:54:45 -0600

    Configurado la funcion del tab layout

--- app/src/main/java/com/example/proyectotorettotrucking/MenuActivity.java
+++ app/src/main/java/com/example/proyectotorettotrucking/MenuActivity.java
@@ -1,14 +1,52 @@
 package com.example.proyectotorettotrucking;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MenuActivity extends AppCompatActivity {
+
+    TabLayout menu;
+    ViewPager2 viewsContainer;
+    FragmentControler menuCtrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        menu = findViewById(R.id.bottom_Tab);
+        viewsContainer = findViewById(R.id.views_container);
+
+        menuCtrl = new FragmentControler(getSupportFragmentManager(),getLifecycle());
+        viewsContainer.setAdapter(menuCtrl);
+        viewsContainer.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                menu.selectTab(menu.getTabAt(position));
+            }
+        });
+        menu.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewsContainer.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
