6556ee550364ecee38b728932f8632ed14af6c50
Javier Cisneros <fjca185@gmail.com>
2022-11-25 21:24:18 -0600

    Login y Strings

--- app/src/main/java/com/example/proyectotorettotrucking/MainActivity.java
+++ app/src/main/java/com/example/proyectotorettotrucking/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.proyectotorettotrucking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText usuario, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void ingresarMenu(View view){
+        Usuario usr = new Usuario();
+        usr.setCorreo(usuario.getText().toString());
+        usr.setContraseña(password.getText().toString());
+        usr.setRegistrado(true);
+        guardarPreferencias(usr);
+        Intent intent = new Intent(MainActivity.this,MenuActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private  void guardarPreferencias(Usuario usr){
+        SharedPreferences preferences = getSharedPreferences("usr.dat",MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("usuario",usr.getCorreo());
+        editor.putString("contraseña",usr.getContraseña());
+        editor.putBoolean("registrado", usr.isRegistrado());
+        editor.apply();
+    }
+    public void salirMenu(View view){
+        finish();
     }
 }
