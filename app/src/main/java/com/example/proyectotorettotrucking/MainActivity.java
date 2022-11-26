73fc933191bc922eb4be923512f698b77eb8c62c
Islu <luigui.angel07@hotmail.com>
2022-11-26 13:55:09 -0600

    Corregido la inicializacion de los edittex

--- app/src/main/java/com/example/proyectotorettotrucking/MainActivity.java
+++ app/src/main/java/com/example/proyectotorettotrucking/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usuario = findViewById(R.id.txtUsuario);
+        password = findViewById(R.id.txtPassword);
 
     }
     public void ingresarMenu(View view){
