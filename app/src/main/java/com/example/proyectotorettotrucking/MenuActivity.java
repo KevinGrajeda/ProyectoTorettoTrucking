41907ff1797933b4005b881f3628f315e749ea68
Islu <luigui.angel07@hotmail.com>
2022-12-03 20:21:02 -0600

    Cambio de boton segun fragment

--- app/src/main/java/com/example/proyectotorettotrucking/MenuActivity.java
+++ app/src/main/java/com/example/proyectotorettotrucking/MenuActivity.java
@@ -14,12 +14,14 @@ import android.widget.Toast;
 
 import com.example.proyectotorettotrucking.baseDeDatos.ControladorBaseDatos;
 import com.example.proyectotorettotrucking.clases.Pedido;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
 public class MenuActivity extends AppCompatActivity {
 
     TabLayout menu;
     ViewPager2 viewsContainer;
+    FloatingActionButton btnFloat;
     FragmentControler menuCtrl;
     ControladorBaseDatos controlador;
     private static MenuActivity instance = null;
@@ -61,13 +63,20 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
         menu = findViewById(R.id.bottom_Tab);
         viewsContainer = findViewById(R.id.views_container);
+        btnFloat = findViewById(R.id.btnAgregar);
         instance = this;
         menuCtrl = new FragmentControler(getSupportFragmentManager(), getLifecycle());
         viewsContainer.setAdapter(menuCtrl);
         viewsContainer.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageSelected(int position) {
+                if (viewsContainer.getCurrentItem() != 2){
+                    btnFloat.setImageResource(R.drawable.ic_baseline_add_24);
+                } else {
+                    btnFloat.setImageResource(R.drawable.ic_done);
+                }
                 menu.selectTab(menu.getTabAt(position));
+
             }
         });
         menu.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
@@ -87,13 +96,14 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.btnAgregar).setOnClickListener(new View.OnClickListener() {
+        btnFloat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (viewsContainer.getCurrentItem() == 2) {
                     ((AgregarPedidoFragment) getSupportFragmentManager().findFragmentByTag("f2")).terminarPedido();
                 } else {
                     viewsContainer.setCurrentItem(2);
+                    btnFloat.setImageResource(R.drawable.ic_done);
                 }
             }
         });
